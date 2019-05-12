575ba61951073d27e77edbf376a96cfa3bc60ec3
Angelo Santagata <angelosantagata@gmail.com>
2019-05-12 23:21:29 +0100

    making code PEP compliant

--- wiser/__init__.py
+++ wiser/__init__.py
@@ -9,25 +9,21 @@ Angelo.santagata@gmail.com
 
 import logging
 import time
-import requests
 from socket import timeout
 from threading import Lock
 import json
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.discovery import load_platform
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_SCAN_INTERVAL,CONF_MINIMUM
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_SCAN_INTERVAL, CONF_MINIMUM
 
-
-# TODO : Once the core library is added to PyPi will modify this
-#REQUIREMENTS = ['package name'']
 
 _LOGGER = logging.getLogger(__name__)
 NOTIFICATION_ID = 'wiser_notification'
 NOTIFICATION_TITLE = 'Wiser Component Setup'
-CONF_BOOST_TEMP_DEFAULT ="20"
-CONF_BOOST_TEMP="boost_temp"
-CONF_BOOST_TEMP_TIME="boost_time"
+CONF_BOOST_TEMP_DEFAULT = "20"
+CONF_BOOST_TEMP = "boost_temp"
+CONF_BOOST_TEMP_TIME = 'boost_time'
 
 DOMAIN = 'wiser'
 DATA_KEY = 'wiser'
@@ -43,15 +39,15 @@ PLATFORM_SCHEMA = vol.Schema({
 
 
 def setup(hass, config):
-    hubHost= config[DOMAIN][0][CONF_HOST]
-    password= config[DOMAIN][0][CONF_PASSWORD]
-    scan_interval= config[DOMAIN][0][CONF_SCAN_INTERVAL].total_seconds()
-    minimum_temp= config[DOMAIN][0][CONF_MINIMUM]
-    boost_temp= config[DOMAIN][0][CONF_BOOST_TEMP]
-    boost_time= config[DOMAIN][0][CONF_BOOST_TEMP_TIME]
+    hub_host = config[DOMAIN][0][CONF_HOST]
+    password = config[DOMAIN][0][CONF_PASSWORD]
+    scan_interval = config[DOMAIN][0][CONF_SCAN_INTERVAL].total_seconds()
+    minimum_temp = config[DOMAIN][0][CONF_MINIMUM]
+    boost_temp = config[DOMAIN][0][CONF_BOOST_TEMP]
+    boost_time = config[DOMAIN][0][CONF_BOOST_TEMP_TIME]
 
-    _LOGGER.info("Wiser setup with HubIp =  {}".format(hubHost))
-    hass.data[DATA_KEY] = WiserHubHandle(hubHost, password, scan_interval,minimum_temp,boost_temp,boost_time)
+    _LOGGER.info("Wiser setup with HubIp =  {}".format(hub_host))
+    hass.data[DATA_KEY] = WiserHubHandle(hub_host, password, scan_interval, minimum_temp, boost_temp, boost_time)
 
     _LOGGER.info("Wiser Component Setup Completed")
 
@@ -59,29 +55,31 @@ def setup(hass, config):
     load_platform(hass, 'sensor', DOMAIN, {}, config)
     return True
 
+
 """
 Single parent class to coordindate the rest calls to teh Heathub
 """
-class WiserHubHandle:
-    
 
-    def __init__(self, ip, secret, scan_interval,minimum_temp,boost_temp,boost_time):
+
+class WiserHubHandle:
+
+    def __init__(self, ip, secret, scan_interval, minimum_temp, boost_temp, boost_time):
         self.scan_interval = scan_interval
         self.ip = ip
         self.secret = secret
-        self.wiserHubInstance=None
+        self.wiserHubInstance = None
         self.mutex = Lock()
-        self.minimum_temp=minimum_temp
+        self.minimum_temp = minimum_temp
         self._updatets = time.time()
-        self.boost_temp=boost_temp
-        self.boost_time=boost_time
+        self.boost_temp = boost_temp
+        self.boost_time = boost_time
 
         _LOGGER.info("min temp = {}".format(self.minimum_temp))
 
     def getHubData(self):
-        from .wiserAPI import wiserHub
-        if (self.wiserHubInstance==None):
-            self.wiserHubInstance=wiserHub.wiserHub(self.ip,self.secret)
+        from wiserHeatingAPI import wiserHub
+        if self.wiserHubInstance is None:
+            self.wiserHubInstance = wiserHub.wiserHub(self.ip, self.secret)
         return self.wiserHubInstance
 
     def getMinimumTemp(self):
@@ -89,44 +87,48 @@ class WiserHubHandle:
 
     def update(self):
         _LOGGER.info("Update Requested")
-        from .wiserAPI import wiserHub
-        if (self.wiserHubInstance==None):
-            self.wiserHubInstance=wiserHub.wiserHub(self.ip,self.secret)
+        from wiserHeatingAPI import wiserHub
+        if self.wiserHubInstance is None:
+            self.wiserHubInstance = wiserHub.wiserHub(self.ip, self.secret)
         with self.mutex:
             if (time.time() - self._updatets) >= self.scan_interval:
                 _LOGGER.debug("*********************************************************************")
-                _LOGGER.info("Scan Interval exceeeded, updating Wiser DataSet from hub")
+                _LOGGER.info("Scan Interval exceeded, updating Wiser DataSet from hub")
                 _LOGGER.debug("*********************************************************************")
                 try:
                     self.wiserHubInstance.refreshData()
                 except timeout as timeoutex:
-                    _LOGGER.error("Timed out whilst connecting to {}, with error {}".format(self.ip,str(timeoutex)))
-                    hass.components.persistent_notification.create("Error: {}<br /> You will need to restart Home Assistant after fixing.".format(ex), title=NOTIFICATION_TITLE, notification_id=NOTIFICATION_ID)
+                    _LOGGER.error("Timed out whilst connecting to {}, with error {}".format(self.ip, str(timeoutex)))
+                    hass.components.persistent_notification.create(
+                        "Error: {}<br /> You will need to restart Home Assistant after fixing.".format(ex),
+                        title=NOTIFICATION_TITLE, notification_id=NOTIFICATION_ID)
                     return False
                 except json.decoder.JSONDecodeError as JSONex:
-                    _LOGGER.error("Data not JSON when getting Data from hub, did you enter the right URL? error {}".format(str(timeoutex)))
-                    hass.components.persistent_notification.create("Error: {}<br /> You will need to restart Home Assistant after fixing.".format(ex), title=NOTIFICATION_TITLE, notification_id=NOTIFICATION_ID)   
+                    _LOGGER.error(
+                        "Data not JSON when getting Data from hub, did you enter the right URL? error {}".format(
+                            str(JSONex)))
+                    hass.components.persistent_notification.create(
+                        "Error: {}<br /> You will need to restart Home Assistant after fixing.".format(ex),
+                        title=NOTIFICATION_TITLE, notification_id=NOTIFICATION_ID)
                     return False
                 self._updatets = time.time()
                 return True
             else:
                 _LOGGER.info("Skipping update (data already gotten within scan interval)")
 
-    def setRoomTemperature(self, roomId, target_temperature):
-        _LOGGER.info("set {} to {}".format(roomId, target_temperature))
-        from .wiserAPI import wiserHub
-        if (self.wiserHubInstance==None):
-            self.wiserHubInstance=wiserHub.wiserHub(self.ip,self.secret)
+    def setRoomTemperature(self, room_id, target_temperature):
+        _LOGGER.info("set {} to {}".format(room_id, target_temperature))
+        from wiserHeatingAPI import wiserHub
+        if self.wiserHubInstance is None:
+            self.wiserHubInstance = wiserHub.wiserHub(self.ip, self.secret)
         with self.mutex:
-            self.wiserHubInstance.setRoomTemperature(roomId, target_temperature)
+            self.wiserHubInstance.setRoomTemperature(room_id, target_temperature)
             return True
-        return False
-    
-    def setRoomMode(self,roomId,mode):
-        from .wiserAPI import wiserHub
-        if (self.wiserHubInstance==None):
-            self.wiserHubInstance=wiserHub.wiserHub(self.ip,self.secret)
+
+    def setRoomMode(self, room_id, mode):
+        from wiserHeatingAPI import wiserHub
+        if self.wiserHubInstance is None:
+            self.wiserHubInstance = wiserHub.wiserHub(self.ip, self.secret)
         with self.mutex:
-            self.wiserHubInstance.setRoomMode(roomId,mode,self.boost_temp,self.boost_time)
+            self.wiserHubInstance.setRoomMode(room_id, mode, self.boost_temp, self.boost_time)
             return True
-        return False
--- wiser/climate.py
+++ wiser/climate.py
@@ -8,49 +8,43 @@ Angelosantagata@gmail.com
 """
 import logging
 
-import voluptuous as vol
+
 # Import the device class from the component that you want to support
 from homeassistant.components.climate import ClimateDevice
 from homeassistant.components.climate.const import (STATE_AUTO, SUPPORT_OPERATION_MODE, SUPPORT_TARGET_TEMPERATURE)
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import ATTR_TEMPERATURE,TEMP_CELSIUS,ATTR_BATTERY_LEVEL
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS, ATTR_BATTERY_LEVEL
 from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
-DOMAIN = "wiser"
+DOMAIN = 'wiser'
 
 STATE_MANUAL = 'manual'
 STATE_BOOST = 'Boost'
-
-
 
 SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE | SUPPORT_OPERATION_MODE
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
-    handler = hass.data[DOMAIN] # Get Handler
-    hubData = handler.getHubData()
+    handler = hass.data[DOMAIN]  # Get Handler
     handler.update()
-    wiserRooms = []
+    wiser_rooms = []
 
     # Get Rooms
     for room in handler.getHubData().getRooms():
-        wiserRooms.append(WiserRoom(room.get('id'),handler))
-    add_devices(wiserRooms)
+        wiser_rooms.append(WiserRoom(room.get('id'), handler))
+    add_devices(wiser_rooms)
 
-    
 
 # Definition of WiserRoom
 class WiserRoom(ClimateDevice):
 
-    def __init__(self, roomId,handler):
+    def __init__(self, room_id, handler):
         """Initialize the sensor."""
         _LOGGER.info('Wiser Room Initialisation')
-        self.handler=handler
-        self.roomId=roomId
-        self._operation_list = [STATE_AUTO, STATE_MANUAL, STATE_BOOST ] 
-
+        self.handler = handler
+        self.roomId = room_id
+        self._operation_list = [STATE_AUTO, STATE_MANUAL, STATE_BOOST]
 
     @property
     def supported_features(self):
@@ -60,10 +54,12 @@ class WiserRoom(ClimateDevice):
     @property
     def should_poll(self):
         return True
+
     @property
     def state(self):
-        _LOGGER.info('State requested for room %s',self.roomId)
+        _LOGGER.info('State requested for room %s', self.roomId)
         return self.handler.getHubData().getRoom(self.roomId).get("Mode")
+
     @property
     def name(self):
         return "Wiser "+self.handler.getHubData().getRoom(self.roomId).get("Name")
@@ -74,10 +70,11 @@ class WiserRoom(ClimateDevice):
 
     @property
     def current_temperature(self):
-        temp=self.handler.getHubData().getRoom(self.roomId).get("CalculatedTemperature")/10
-        if temp< self.handler.getMinimumTemp():
-            # Sometimes we get really low temps (like -3000!), not sure why, if we do then just set it to -20 for now till i debug this.
-            temp= self.handler.getMinimumTemp()
+        temp = self.handler.getHubData().getRoom(self.roomId).get("CalculatedTemperature")/10
+        if temp < self.handler.getMinimumTemp():
+            # Sometimes we get really low temps (like -3000!), not sure why,
+            # if we do then just set it to -20 for now till i debug this.
+            temp = self.handler.getMinimumTemp()
         return temp 
 
     @property
@@ -90,7 +87,7 @@ class WiserRoom(ClimateDevice):
 
     @property
     def target_temperature(self):
-          return self.handler.getHubData().getRoom(self.roomId).get("CurrentSetPoint")/10
+        return self.handler.getHubData().getRoom(self.roomId).get("CurrentSetPoint")/10
     
     @property
     def operation_list(self):
@@ -107,12 +104,12 @@ class WiserRoom(ClimateDevice):
     @property
     def state_attributes(self):
         # Generic attributes
-        attrs= super().state_attributes
+        attrs = super().state_attributes
         attrs['percentage_demand'] = self.handler.getHubData().getRoom(self.roomId).get("PercentageDemand")
         attrs['heating_rate'] = self.handler.getHubData().getRoom(self.roomId).get("HeatingRate")
         attrs['window_state'] = self.handler.getHubData().getRoom(self.roomId).get("WindowState")
-        attrs['window_detection_active']= self.handler.getHubData().getRoom(self.roomId).get("WindowDetectionActive")
-        attrs['away_mode_supressed']= self.handler.getHubData().getRoom(self.roomId).get("AwayModeSuppressed")
+        attrs['window_detection_active'] = self.handler.getHubData().getRoom(self.roomId).get("WindowDetectionActive")
+        attrs['away_mode_supressed'] = self.handler.getHubData().getRoom(self.roomId).get("AwayModeSuppressed")
         return attrs 
 
     # Set temperature
@@ -121,14 +118,15 @@ class WiserRoom(ClimateDevice):
         if kwargs.get(ATTR_TEMPERATURE) is None:
             return False
         target_temperature = kwargs.get(ATTR_TEMPERATURE)
-        _LOGGER.debug("Setting Device Temperature for roomId {}, temperature {}".format(self.roomId,target_temperature))
+        _LOGGER.debug("Setting Device Temperature for roomId {}, temperature {}".
+                      format(self.roomId, target_temperature))
         _LOGGER.debug("Value of wiserhub {}".format(self.handler))
-        self.handler.setRoomTemperature(self.roomId,target_temperature)
+        self.handler.setRoomTemperature(self.roomId, target_temperature)
         
     def set_operation_mode(self, operation_mode):
         """Set new operation mode."""
-        _LOGGER.debug("*******Setting Device Operation {} for roomId {}".format(operation_mode,self.roomId))
-        self.handler.setRoomMode(self.roomId,operation_mode)
+        _LOGGER.debug("*******Setting Device Operation {} for roomId {}".format(operation_mode, self.roomId))
+        self.handler.setRoomMode(self.roomId, operation_mode)
         return True
 
 
